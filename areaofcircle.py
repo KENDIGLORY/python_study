cf761290cb1f6320c1492ffdc5e49382aad4cefa
KENDIGLORY <glorykendi@gmail.com>
2024-10-15 19:12:53 +0300

    first commit

---
+++ areaofcircle.py
@@ -0,0 +1,4 @@
+radius=100
+pi=3.142
+area=pi*radius*radius
+print(area)
---
+++ intro.py
@@ -0,0 +1,32 @@
+#print("glory kendi")
+#print("100")
+#print("10+20")
+#print("100-50")
+myname="glory kendi"
+myname="lucy abdi"
+print(myname)
+print(myname)
+num=100
+print(num)
+
+#declare 2 variables num 1 and num2 add them up in a variable sum then print sum
+num1=100
+num2=400
+sum=num1+num2
+print(sum)
+
+fname="glory"
+lname="kendi"
+fullname=fname+" "+lname
+print(fullname)
+
+# declare a variable phone_number get the input from the terminal
+phone_number=input("enter number: ")
+print(phone_number)
+
+print("titus sammy")
+print("sunday service")
+print("150")
+print("100+50")
+print(100-50)
+print(100+100)
