7751386f7401f2e8006a3718e73c482a9c03a7ea
KENDIGLORY <glorykendi@gmail.com>
2024-10-16 20:38:25 +0300

    learnt string methods

--- strings.py
+++ strings.py
@@ -2,9 +2,37 @@
 #print(fname)
 
 
-myname="glory"
-print(type(myname))
-num="1000"
-print(type(num))
-num1=1000
-print(type(num1))
+#myname="glory"
+#print(type(myname))
+#num="1000"
+#print(type(num))
+#num1=1000
+#print(type(num1))
+
+
+fname="glory"
+sname="kendi"
+full_name=fname+" "+sname
+print(full_name)
+
+text="python class"
+print(text[10])
+
+text="python class for beginners"
+text1=text.capitalize ()
+print(text1)
+text2=text.upper ()
+print(text2)
+text3=text.lower()
+print(text3)
+
+text= "      glorykendi@gmail.com    "
+text1=text.strip()
+print(text1)
+text2="glorykendi@gmail.com"
+text3=text2.split("@")
+print(text3)
+text4="python class for beginners"
+print(text4[7:12])
+
+email="glorykendi@gmail.com"
