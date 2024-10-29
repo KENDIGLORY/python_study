4df0ed81bee7d7cc7ed025f82638cf6da1f96de2
KENDIGLORY <glorykendi@gmail.com>
2024-10-29 18:36:57 +0200

    conditonal statements done

--- extra_statements.py
+++ extra_statements.py
@@ -26,9 +26,45 @@ if len(str1)> len(str2):
 elif len(str1)==len(str2):
    longer='both are equal'
 else:
-   longer=f'{str2} is longer'
+   longer=f'{str2} is longer' 
 
 print(longer)
 
    
-     
+# 3.Given a list valid_ids = [101, 102, 103] and a variable user_id = 105,
+#  write a conditional statement that:
+# Prints "Access Granted" if user_id is in valid_ids.
+# Prints "Access Denied" if user_id is not in valid_ids.
+valid_ids=[101, 102, 103]
+user_id = 105
+if user_id in valid_ids:
+   print('Access Granted')
+else:
+   print('Access Denied')
+
+# 4.Given a variable value that could be of any type, write a conditional statement that:
+# Prints "String Detected" if value is a string.
+# Prints "Integer Detected" if value is an integer.
+# c for any other type.
+value=5000
+if type (value)==str:
+   print('String Detected')
+elif type (value)==int:
+   print('Integer Detected')
+else:
+    print('unknown type')
+# 5.Given x = 7 and y = 14, write nested conditional statements that print:
+# "x and y are both even" if both x and y are even numbers.
+# "Only y is even" if only y is even.
+# "Neither x nor y are even" if both are odd.
+
+    x=7
+    y=14
+    if x%2==0:
+       if y%2==0:
+          print('x and y are both even')
+       else:
+          if y%2==0:
+           print('only y is even')
+    else:
+      print('neither x nor y are even')
