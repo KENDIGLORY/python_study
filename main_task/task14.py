3579c8f0129404ebacfd183bf04951d24aaf07bc
KENDIGLORY <glorykendi@gmail.com>
2024-11-03 22:27:14 +0200

    updated main_task

--- main_task/task14.py
+++ main_task/task14.py
@@ -5,5 +5,12 @@
 # and take the user to re-enter the inputs .
 # Once you learn functions,revisit this and write this code inside a function.
 
-
-
+value_a=input("enter first number: ")
+value_b=input("enter second number: ")
+sum=(value_a) + (value_b)
+if value_a==int() or float() and value_b==float() or int():
+    print("acccept input")
+else:
+    print("invalid character entered")
+    
+print(sum)
